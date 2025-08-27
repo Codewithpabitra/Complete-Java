b66d6345d2c462719ee0fdbc440733a29fd93fd8
Codewithpabitra <pabitra05.maity@gmail.com>
2025-08-27 21:32:28 +0530

    day 04 done

---
+++ Day 04/Demo.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Demo {
+    
+    public static void main(String[] args) {
+        // System.out.print("Hello hello");
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+         System.out.print(name);
+
+    }
+}
+
+
+
+
---
+++ Day 04/practice_01.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class practice_01 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        System.out.print("Heye hey");
+//        System.out.print("Heye hey");
+
+        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextInt());
+
+//        int x = input.nextInt();
+//        System.out.println(x);
+
+//        float f = input.nextFloat();
+//        float fb = 12.34f;
+//
+//        double db = 123.5676565;
+//        char ch = '@';
+//
+//        long lg = 647368365;
+
+        // String input
+//        String name = input.nextLine();
+//        System.out.println(name);
+
+        float num = 16.345f;
+        int intNum = (int)(num);
+//        System.out.println(intNum);
+
+        int val = 'A';// 65
+//        System.out.println(val);
+
+        int num1 = 23;
+        int num2 = 12;
+        int sum = num1 + num2;
+        System.out.println("The sum of the values " + sum);
+
+
+
+
+
+    }
+
+}
